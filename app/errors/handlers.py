99e5998242da186d999c52742dbd5a141ce2b370
Hendrik L <hendrik+dev@xd0.de>
2024-07-11 14:04:25 +0200

    enable 404 page again

--- app/errors/handlers.py
+++ app/errors/handlers.py
@@ -1,14 +1,20 @@
-from flask import render_template
+from flask import render_template, g, request
 from app import db
 from app.errors import bp
 
 
 # 404 error handler removed because a lot of 404s are just images in /static/* and it doesn't make sense to waste cpu cycles presenting a nice page.
 # Also rendering a page requires populating g.site which means hitting the DB.
-# @bp.app_errorhandler(404)
-# def not_found_error(error):
-#     return render_template('errors/404.html'), 404
-
+@bp.app_errorhandler(404)
+def not_found_error(error):
+    if request.path.startswith('/u/') or request.path.startswith('/c/'):
+        return render_template('errors/404.html'), 404
+    else:
+        return """<!doctype html>
+                  <html lang=en>
+                  <title>404 Not Found</title>
+                  <h1>Not Found</h1>
+                  <p>The requested URL was not found on the server. If you entered the URL manually please check your spelling and try again.</p>""", 404
 
 @bp.app_errorhandler(500)
 def internal_error(error):
