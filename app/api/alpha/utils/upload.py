42d9aafa847a1171193c98b31926f9e43e27a8d0
Luca Schnellmann <oss@lusc.ch>
2025-12-27 18:28:03 +0100

    Associate API image uploads to user

--- app/api/alpha/utils/upload.py
+++ app/api/alpha/utils/upload.py
@@ -30,14 +30,14 @@ def post_upload_image(auth, image_file=None):
 
 
 def post_upload_community_image(auth, image_file=None):
-    authorise_api_user(auth)
-    url = process_upload(image_file, destination='communities')
+    user_id = authorise_api_user(auth)
+    url = process_upload(image_file, destination='communities', user_id=user_id)
     return {'url': url}
 
 
 def post_upload_user_image(auth, image_file=None):
-    authorise_api_user(auth)
-    url = process_upload(image_file, destination='users')
+    user_id = authorise_api_user(auth)
+    url = process_upload(image_file, destination='users', user_id=user_id)
     return {'url': url}
 
 
