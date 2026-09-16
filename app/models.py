dd2e9f9603be016f768b45ede0a71f1f0abd0cbd
rimu <rimu@rimu.geek.nz>
2026-09-16 19:05:49 +1200

    ensure rss bot can post in mod-only communities

--- app/models.py
+++ app/models.py
@@ -1247,7 +1247,7 @@ class User(UserMixin, db.Model):
             return False
 
     def is_rss_bot(self):
-        return self.bot and self.is_local() and self.user_name == 'feed_bot'
+        return self.bot and self.user_name == 'feed_bot'
 
     def trustworthy(self):
         if self.is_admin():
